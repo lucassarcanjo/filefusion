88442bae898c1cc8ce2ac4da08a2c337dd1146e3
Lucas Arcanjo <lucassarcanjo@gmail.com>
2023-05-06 23:11:20 -0300

    Add Cosmos DB

--- terraform/main.tf
+++ terraform/main.tf
@@ -71,3 +71,38 @@ resource "azurerm_linux_function_app" "app" {
     }
   }
 }
+
+// Cosmos Database
+resource "azurerm_cosmosdb_account" "cosmos" {
+  name = "filefusion-cosmos"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  offer_type = "Standard"
+  kind = "GlobalDocumentDB"
+  enable_automatic_failover = false
+  enable_free_tier = true
+  geo_location {
+    location = azurerm_resource_group.rg.location
+    failover_priority = 0
+  }
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
+  }
+}
+
+resource "azurerm_cosmosdb_sql_database" "db" {
+  name = "db"
+  resource_group_name = azurerm_resource_group.rg.name
+  account_name = azurerm_cosmosdb_account.cosmos.name
+}
+
+resource "azurerm_cosmosdb_sql_container" "container" {
+  name = "actions"
+  resource_group_name = azurerm_resource_group.rg.name
+  account_name = azurerm_cosmosdb_account.cosmos.name
+  database_name = azurerm_cosmosdb_sql_database.db.name
+  partition_key_path = "/definition/id"
+  partition_key_version = 1
+}
