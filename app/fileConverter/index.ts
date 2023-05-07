848185975a7cb05a89fbede2f445a4f373c5527d
Lucas Arcanjo <lucassarcanjo@gmail.com>
2023-05-07 00:21:25 -0300

    Add fileConverter function

--- app/fileConverter/index.ts
+++ app/fileConverter/index.ts
@@ -1,19 +1,67 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import * as sharp from "sharp";
+import HTTP_CODES from "http-status-enum";
+import { generateReadOnlySASUrl } from "./azure-storage-blob-sas-url";
 
 const httpTrigger: AzureFunction = async function (
   context: Context,
   req: HttpRequest
-): Promise<void> {
-  context.log("HTTP trigger function processed a request.");
-  const name = req.query.name || (req.body && req.body.name);
-  const responseMessage = name
-    ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-    : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+) {
+  const storageConnectionString = process.env.AzureWebJobsStorage;
+  if (!storageConnectionString) {
+    context.res.body = `AzureWebJobsStorage env var is not defined - get Storage Connection string from Azure portal`;
+    context.res.status = HTTP_CODES.BAD_REQUEST;
+    return context.res;
+  }
 
-  context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: responseMessage,
-  };
+  const fileName = req.query?.filename;
+  if (!fileName) {
+    context.res = {
+      status: HTTP_CODES.BAD_REQUEST,
+      body: "filename is required",
+    };
+    return context.res;
+  }
+
+  if (!req.body || !req.body.image) {
+    context.res = {
+      status: HTTP_CODES.BAD_REQUEST,
+      body: "image property is required",
+    };
+    return context.res;
+  }
+
+  context.log(
+    `Filename: ${req.query.filename}, Content type:${req.headers["content-type"]}`
+  );
+
+  try {
+    const bodyBuffer = Buffer.from(req.body.image, "base64");
+
+    const convertedImage = await sharp(bodyBuffer).png().toBuffer();
+
+    context.bindings.storage = convertedImage;
+
+    const sasInfo = await generateReadOnlySASUrl(
+      process.env.AzureWebJobsStorage,
+      "app-files",
+      fileName
+    );
+
+    context.res = {
+      status: HTTP_CODES.ACCEPTED,
+      body: {
+        fileName,
+        url: sasInfo.accountSasTokenUrl,
+      },
+    };
+  } catch (err) {
+    context.log.error(err.message);
+    context.res.body = { error: `${err.message}` };
+    context.res.status = HTTP_CODES.INTERNAL_SERVER_ERROR;
+  }
+
+  return context.res;
 };
 
 export default httpTrigger;
