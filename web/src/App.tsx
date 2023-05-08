cd4458ecf4ae54d8982a797d0914576a37794352
Lucas Arcanjo <lucassarcanjo@gmail.com>
2023-05-08 00:18:40 -0300

    Improve UI for web app

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef } from "react";
+import { Player } from "@lottiefiles/react-lottie-player";
+
+import fileMoving from "./assets/file-moving.json";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const playerRef = useRef<Player>(null);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main className="h-screen bg-gradient-to-b from-[#EEF4FA] to-[#CFE2F7] flex items-center justify-center">
+      <section
+        className="bg-white p-7 rounded-3xl shadow-xl h-[400px] w-[600px]"
+        onMouseEnter={() => playerRef.current?.play()}
+      >
+        <div className="rounded-[20px] bg-[#F2F6FC] w-full h-full border-2 border-dashed border-[#BBC9D7] flex items-center justify-center flex-col p-4">
+          <div className="w-[250px] h-[250px]">
+            <Player ref={playerRef} src={fileMoving} />
+          </div>
+          <p className="text-lg  text-center text-muted-foreground">
+            Arraste aqui as imagens que você deseja converter. Elas vão ser
+            enviados automaticamente.
+          </p>
+        </div>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import App from "./App.tsx";
 import { TelemetryProvider } from "./utils/telemetry.tsx";
-import "./index.css";
+import "./styles/global.css";
+// import "@fontsource/roboto";
+// import "@fontsource/inter";
+import "@fontsource/raleway/500.css";
+import "@fontsource/raleway/600.css";
+import "@fontsource/raleway/700.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
