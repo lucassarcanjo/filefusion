edff57f151cdd01a8a6ca4226070e5c3bd906777
Lucas Arcanjo <lucassarcanjo@gmail.com>
2023-05-11 00:04:33 -0300

    Add state management to upload files

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,7 @@
-import { useRef, useState } from "react";
+import { useEffect, useReducer, useRef, useState } from "react";
 import { Player } from "@lottiefiles/react-lottie-player";
 import { FileImage } from "lucide-react";
 
-import fileMoving from "./assets/file-moving.json";
 import { Separator } from "./components/Separator";
 import { Button } from "./components/Button";
 import {
@@ -13,12 +12,83 @@ import {
   SelectValue,
 } from "./components/Select";
 
+import fileMoving from "./assets/file-moving.json";
+import loading from "./assets/loading.json";
+import alert from "./assets/alertOctagon.json";
+import download from "./assets/download.json";
+
+type ImageFormat = "png" | "jpeg" | "gif" | "bmp" | "tiff";
+
+interface ConversionOperation {
+  file: File;
+  format: ImageFormat | undefined;
+  result: string | null;
+  loading: boolean;
+  error: boolean | null;
+}
+
+const initalState: ConversionOperation[] = [];
+
+const reducer = (
+  state: ConversionOperation[],
+  action: any
+): ConversionOperation[] => {
+  switch (action.type) {
+    case "SET_FILES":
+      return [
+        ...state,
+        ...action.payload.map((file: File) => ({
+          file,
+          format: "",
+          result: null,
+          loading: false,
+          error: null,
+        })),
+      ];
+
+    case "SET_FORMAT":
+      return state.map((file, index) =>
+        index === action.payload.index
+          ? { ...file, format: action.payload.format }
+          : file
+      );
+
+    case "SET_RESULT":
+      return state.map((file, index) =>
+        index === action.payload.index
+          ? { ...file, result: action.payload.result }
+          : file
+      );
+
+    case "SET_LOADING":
+      return state.map((file, index) =>
+        index === action.payload.index
+          ? { ...file, loading: action.payload.loading }
+          : file
+      );
+
+    case "SET_ERROR":
+      return state.map((file, index) =>
+        index === action.payload.index
+          ? { ...file, error: action.payload.error }
+          : file
+      );
+
+    default:
+      return state;
+  }
+};
+
 function App() {
   const [dragActive, setDragActive] = useState(false);
-  const [files, setFiles] = useState<File[]>([]);
+  const [conversionFiles, dispatch] = useReducer(reducer, initalState);
 
   const playerRef = useRef<Player>(null);
   const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleFiles = (files: FileList) => {
+    dispatch({ type: "SET_FILES", payload: Array.from(files) });
+  };
 
   const handleDrag = (event: React.DragEvent<HTMLElement>) => {
     event.preventDefault();
@@ -37,21 +107,58 @@ function App() {
     setDragActive(false);
 
     if (event.dataTransfer.files && event.dataTransfer.files[0]) {
-      // handleFiles(e.dataTransfer.files);
-      console.log("drag");
-
-      setFiles(Array.from(event.dataTransfer.files));
+      handleFiles(event.dataTransfer.files);
     }
   };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files[0]) {
-      // handleFiles(e.target.files);
-      console.log("change");
-
-      setFiles(Array.from(event.target.files));
+      handleFiles(event.target.files);
     }
   };
+
+  useEffect(() => {
+    if (conversionFiles.length === 0) {
+      playerRef.current?.play();
+      return;
+    }
+
+    conversionFiles.forEach((file, index) => {
+      if (!file.format) return;
+      if (file.result !== null) return;
+      if (file.loading) return;
+      if (file.error !== null) return;
+
+      dispatch({ type: "SET_LOADING", payload: { index, loading: true } });
+
+      const formData = new FormData();
+      formData.append("image", file.file);
+      formData.append("format", file.format);
+
+      fetch("http://localhost:3001/convert", {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => response.blob())
+        .then((blob) => {
+          const url = URL.createObjectURL(blob);
+          dispatch({
+            type: "SET_RESULT",
+            payload: { index, result: url },
+          });
+        })
+        .catch((error) => {
+          console.error(error);
+          dispatch({
+            type: "SET_ERROR",
+            payload: { index, error: true },
+          });
+        })
+        .finally(() => {
+          dispatch({ type: "SET_LOADING", payload: { index, loading: false } });
+        });
+    });
+  }, [conversionFiles]);
 
   return (
     <main className="h-screen bg-gradient-to-b from-[#EEF4FA] to-[#CFE2F7] flex items-center justify-center">
@@ -100,17 +207,27 @@ function App() {
             </label>
           </div>
         </form>
-        {files.length > 0 && (
+        {conversionFiles.length > 0 && (
           <div className="mt-4 ">
             <ul className="mt-2 flex flex-col gap-4">
-              {files.map((file) => (
+              {conversionFiles.map(({ file, format }, index) => (
                 <li
                   key={file.name}
                   className="text-sm bg-[#F9F9F9] rounded-xl p-4 flex gap-4 items-center"
                 >
                   <FileImage size={20} className="text-[#BBC9D7]" />
+
                   <span className="mr-auto">{file.name}</span>
-                  <Select>
+
+                  <Select
+                    value={format}
+                    onValueChange={(value: ImageFormat) =>
+                      dispatch({
+                        type: "SET_FORMAT",
+                        payload: { index, format: value },
+                      })
+                    }
+                  >
                     <SelectTrigger className="w-[180px]">
                       <SelectValue placeholder="Formato" />
                     </SelectTrigger>
@@ -122,32 +239,27 @@ function App() {
                       <SelectItem value="tiff">TIFF</SelectItem>
                     </SelectContent>
                   </Select>
-                  <svg
-                    className="animate-spin -ml-1 mr-3 h-5 w-5 text-[#3A55F3]"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
-                    <circle
-                      className="opacity-25"
-                      cx="12"
-                      cy="12"
-                      r="10"
-                      stroke="currentColor"
-                      stroke-width="4"
-                    ></circle>
-                    <path
-                      className="opacity-75"
-                      fill="currentColor"
-                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                    ></path>
-                  </svg>
+
+                  {conversionFiles[index].loading && (
+                    <Player src={loading} autoplay loop />
+                  )}
+
+                  {conversionFiles[index].result && (
+                    <Player src={download} autoplay loop />
+                  )}
+
+                  {conversionFiles[index].error && (
+                    <Player src={alert} autoplay loop />
+                  )}
                 </li>
               ))}
             </ul>
             {/* <p className="mt-6 text-sm text-muted-foreground text-right">
               {files.length} arquivos selecionados
             </p> */}
+            {/* <div className="mt-6 text-right">
+              <Button variant="secondary">Enviar</Button>
+            </div> */}
           </div>
         )}
       </section>
