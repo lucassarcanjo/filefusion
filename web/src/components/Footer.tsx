69f88bcf8981915188759f7c88662e4968821767
Lucas Arcanjo <lucassarcanjo@gmail.com>
2023-05-13 18:20:59 -0300

    Add hover effect to footer

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -1,8 +1,8 @@
 const repoUrl = "https://github.com/lucassarcanjo/filefusion";
 
 export const Footer = () => (
-  <div className="w-full h-16 rounded-t-2xl bg-white shadow-xl flex justify-center items-center">
-    <a href={repoUrl} target="_blank">
+  <div className="w-full h-16 rounded-t-2xl bg-white text-muted-foreground shadow-xl flex justify-center items-center">
+    <a href={repoUrl} target="_blank" className="hover:opacity-80">
       Made with 💙 using GitHub
     </a>
   </div>
