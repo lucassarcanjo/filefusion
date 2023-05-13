903043b75c8958b9d39c3649f92e7ff1291344a2
Lucas Arcanjo <lucassarcanjo@gmail.com>
2023-05-13 12:53:49 -0300

    Add footer with github repo

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -20,6 +20,7 @@ import fileMoving from "./assets/file-moving.json";
 import loadingAnimation from "./assets/loading.json";
 import alert from "./assets/alertOctagon.json";
 import download from "./assets/download.json";
+import { Footer } from "./components/Footer";
 
 type ImageFormat =
   | "image/png"
@@ -186,126 +187,131 @@ function App() {
   }, [conversionFiles]);
 
   return (
-    <main className="h-screen bg-gradient-to-b from-[#EEF4FA] to-[#CFE2F7] flex items-center justify-center">
-      <section
-        className="bg-white p-7 rounded-3xl shadow-xl w-[600px] relative"
-        onMouseEnter={() => playerRef.current?.play()}
-      >
-        <form onDragEnter={handleDrag} onSubmit={(e) => e.preventDefault()}>
-          <div className="rounded-[20px] bg-[#F2F6FC] w-full h-full border-2 border-dashed border-[#BBC9D7] flex items-center justify-center flex-col py-6 px-4">
-            <div className="w-[250px] h-[250px]">
-              <Player ref={playerRef} src={fileMoving} />
-            </div>
-            <p className="text-lg  text-center text-muted-foreground">
-              Arraste aqui as imagens que você deseja converter. Elas serão
-              enviadas automaticamente.
-            </p>
+    <div className="bg-gradient-to-b from-[#EEF4FA] to-[#CFE2F7]">
+      <main className="h-screen  flex items-center justify-center">
+        <section
+          className="bg-white p-7 rounded-3xl shadow-xl w-[600px] relative"
+          onMouseEnter={() => playerRef.current?.play()}
+        >
+          <form onDragEnter={handleDrag} onSubmit={(e) => e.preventDefault()}>
+            <div className="rounded-[20px] bg-[#F2F6FC] w-full h-full border-2 border-dashed border-[#BBC9D7] flex items-center justify-center flex-col py-6 px-4">
+              <div className="w-[250px] h-[250px]">
+                <Player ref={playerRef} src={fileMoving} />
+              </div>
+              <p className="text-lg  text-center text-muted-foreground">
+                Arraste aqui as imagens que você deseja converter. Elas serão
+                enviadas automaticamente.
+              </p>
 
-            <Separator className="my-4 max-w-xs" />
+              <Separator className="my-4 max-w-xs" />
 
-            <label htmlFor="picture-input">
-              <Button
-                className="bg-[#3A55F3] font-bold"
-                onClick={() => fileInputRef.current?.click()}
-              >
-                Procurar arquivos
-              </Button>
-              <input
-                multiple
-                id="picture-input"
-                type="file"
-                className="hidden"
-                ref={fileInputRef}
-                onChange={handleChange}
-                accept="image/png, image/jpeg, image/jpg, image/gif, image/bmp, image/tiff"
-              />
-
-              {dragActive && (
-                <div
-                  className="absolute top-0 left-0 w-full h-full bg-[#3A55F3] bg-opacity-10 rounded-[20px]"
-                  onDragEnter={handleDrag}
-                  onDragOver={handleDrag}
-                  onDragLeave={handleDrag}
-                  onDrop={handleDrop}
+              <label htmlFor="picture-input">
+                <Button
+                  className="bg-[#3A55F3] font-bold"
+                  onClick={() => fileInputRef.current?.click()}
+                >
+                  Procurar arquivos
+                </Button>
+                <input
+                  multiple
+                  id="picture-input"
+                  type="file"
+                  className="hidden"
+                  ref={fileInputRef}
+                  onChange={handleChange}
+                  accept="image/png, image/jpeg, image/jpg, image/gif, image/bmp, image/tiff"
                 />
-              )}
-            </label>
-          </div>
-        </form>
-        {conversionFiles.length > 0 && (
-          <div className="mt-4 ">
-            <ul className="mt-2 flex flex-col gap-4">
-              {conversionFiles.map(
-                ({ file, format, result, loading }, index) => (
-                  <li
-                    key={file.name}
-                    className="text-sm bg-[#F9F9F9] rounded-xl p-4 flex gap-4 items-center"
-                  >
-                    <FileImage size={20} className="text-[#BBC9D7]" />
 
-                    <span className="mr-auto">{file.name}</span>
-
-                    <Select
-                      value={format}
-                      onValueChange={(value: ImageFormat) =>
-                        dispatch({
-                          type: "SET_FORMAT",
-                          payload: { index, format: value },
-                        })
-                      }
+                {dragActive && (
+                  <div
+                    className="absolute top-0 left-0 w-full h-full bg-[#3A55F3] bg-opacity-10 rounded-[20px]"
+                    onDragEnter={handleDrag}
+                    onDragOver={handleDrag}
+                    onDragLeave={handleDrag}
+                    onDrop={handleDrop}
+                  />
+                )}
+              </label>
+            </div>
+          </form>
+          {conversionFiles.length > 0 && (
+            <div className="mt-4 ">
+              <ul className="mt-2 flex flex-col gap-4">
+                {conversionFiles.map(
+                  ({ file, format, result, loading }, index) => (
+                    <li
+                      key={file.name}
+                      className="text-sm bg-[#F9F9F9] rounded-xl p-4 flex gap-4 items-center"
                     >
-                      <SelectTrigger className="w-[180px]">
-                        <SelectValue placeholder="Formato" />
-                      </SelectTrigger>
+                      <FileImage size={20} className="text-[#BBC9D7]" />
 
-                      {/* TODO: remove the current format of file */}
-                      <SelectContent>
-                        <SelectItem value="image/png">PNG</SelectItem>
-                        <SelectItem value="image/jpeg">JPEG</SelectItem>
-                        <SelectItem value="image/gif">GIF</SelectItem>
-                        <SelectItem value="image/bmp">BMP</SelectItem>
-                        <SelectItem value="image/tiff">TIFF</SelectItem>
-                      </SelectContent>
-                    </Select>
+                      <span className="mr-auto">{file.name}</span>
 
-                    {loading && <Player src={loadingAnimation} autoplay loop />}
+                      <Select
+                        value={format}
+                        onValueChange={(value: ImageFormat) =>
+                          dispatch({
+                            type: "SET_FORMAT",
+                            payload: { index, format: value },
+                          })
+                        }
+                      >
+                        <SelectTrigger className="w-[180px]">
+                          <SelectValue placeholder="Formato" />
+                        </SelectTrigger>
 
-                    {result && (
-                      <Tooltip delayDuration={0}>
-                        <TooltipTrigger>
-                          <Button
-                            variant="ghost"
-                            onClick={() => downloadRef.current?.play()}
-                          >
-                            <a href={result}>
-                              <Player ref={downloadRef} src={download} />
-                            </a>
-                          </Button>
-                        </TooltipTrigger>
-                        <TooltipContent>
-                          <p>Clique aqui para baixar seu arquivo</p>
-                        </TooltipContent>
-                      </Tooltip>
-                    )}
+                        {/* TODO: remove the current format of file */}
+                        <SelectContent>
+                          <SelectItem value="image/png">PNG</SelectItem>
+                          <SelectItem value="image/jpeg">JPEG</SelectItem>
+                          <SelectItem value="image/gif">GIF</SelectItem>
+                          <SelectItem value="image/bmp">BMP</SelectItem>
+                          <SelectItem value="image/tiff">TIFF</SelectItem>
+                        </SelectContent>
+                      </Select>
 
-                    {conversionFiles[index].error && (
-                      <Tooltip delayDuration={0}>
-                        <TooltipTrigger>
-                          <Player src={alert} autoplay loop />
-                        </TooltipTrigger>
-                        <TooltipContent>
-                          <p>{conversionFiles[index].error}</p>
-                        </TooltipContent>
-                      </Tooltip>
-                    )}
-                  </li>
-                )
-              )}
-            </ul>
-          </div>
-        )}
-      </section>
-    </main>
+                      {loading && (
+                        <Player src={loadingAnimation} autoplay loop />
+                      )}
+
+                      {result && (
+                        <Tooltip delayDuration={0}>
+                          <TooltipTrigger>
+                            <Button
+                              variant="ghost"
+                              onClick={() => downloadRef.current?.play()}
+                            >
+                              <a href={result}>
+                                <Player ref={downloadRef} src={download} />
+                              </a>
+                            </Button>
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <p>Clique aqui para baixar seu arquivo</p>
+                          </TooltipContent>
+                        </Tooltip>
+                      )}
+
+                      {conversionFiles[index].error && (
+                        <Tooltip delayDuration={0}>
+                          <TooltipTrigger>
+                            <Player src={alert} autoplay loop />
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <p>{conversionFiles[index].error}</p>
+                          </TooltipContent>
+                        </Tooltip>
+                      )}
+                    </li>
+                  )
+                )}
+              </ul>
+            </div>
+          )}
+        </section>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
---
+++ web/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const repoUrl = "https://github.com/lucassarcanjo/filefusion";
+
+export const Footer = () => (
+  <div className="w-full h-16 rounded-t-2xl bg-white shadow-xl flex justify-center items-center">
+    <a href={repoUrl} target="_blank">
+      Made with 💙 using GitHub
+    </a>
+  </div>
+);
