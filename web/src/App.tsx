1b27a487c3779407c52e8ac960b21180e2f423b7
Lucas Arcanjo <lucassarcanjo@gmail.com>
2023-05-13 12:21:31 -0300

    Connect frontend with backend to convert images

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,13 @@
 import { useEffect, useReducer, useRef, useState } from "react";
 import { Player } from "@lottiefiles/react-lottie-player";
 import { FileImage } from "lucide-react";
+import wretch from "wretch";
+import { v4 as uuid } from "uuid";
 
 import { Separator } from "./components/Separator";
 import { Button } from "./components/Button";
+import { Tooltip, TooltipContent } from "./components/Tooltip";
+import { TooltipTrigger } from "./components/Tooltip";
 import {
   Select,
   SelectContent,
@@ -13,11 +17,16 @@ import {
 } from "./components/Select";
 
 import fileMoving from "./assets/file-moving.json";
-import loading from "./assets/loading.json";
+import loadingAnimation from "./assets/loading.json";
 import alert from "./assets/alertOctagon.json";
 import download from "./assets/download.json";
 
-type ImageFormat = "png" | "jpeg" | "gif" | "bmp" | "tiff";
+type ImageFormat =
+  | "image/png"
+  | "image/jpeg"
+  | "image/gif"
+  | "image/bmp"
+  | "image/tiff";
 
 interface ConversionOperation {
   file: File;
@@ -25,6 +34,11 @@ interface ConversionOperation {
   result: string | null;
   loading: boolean;
   error: boolean | null;
+}
+
+interface ConversionResponse {
+  url: string;
+  fileName: string;
 }
 
 const initalState: ConversionOperation[] = [];
@@ -56,7 +70,7 @@ const reducer = (
     case "SET_RESULT":
       return state.map((file, index) =>
         index === action.payload.index
-          ? { ...file, result: action.payload.result }
+          ? { ...file, result: action.payload.result, loading: false }
           : file
       );
 
@@ -70,7 +84,7 @@ const reducer = (
     case "SET_ERROR":
       return state.map((file, index) =>
         index === action.payload.index
-          ? { ...file, error: action.payload.error }
+          ? { ...file, error: action.payload.error, loading: false }
           : file
       );
 
@@ -84,6 +98,7 @@ function App() {
   const [conversionFiles, dispatch] = useReducer(reducer, initalState);
 
   const playerRef = useRef<Player>(null);
+  const downloadRef = useRef<Player>(null);
   const fileInputRef = useRef<HTMLInputElement>(null);
 
   const handleFiles = (files: FileList) => {
@@ -131,32 +146,42 @@ function App() {
 
       dispatch({ type: "SET_LOADING", payload: { index, loading: true } });
 
-      const formData = new FormData();
-      formData.append("image", file.file);
-      formData.append("format", file.format);
+      const reader = new FileReader();
+      reader.readAsDataURL(file.file);
+      reader.onload = async () => {
+        const encodedImage = reader.result?.toString().split(",")[1];
+        const filename = `${uuid()}.${file.format?.split("/")[1]}`;
 
-      fetch("http://localhost:3001/convert", {
-        method: "POST",
-        body: formData,
-      })
-        .then((response) => response.blob())
-        .then((blob) => {
-          const url = URL.createObjectURL(blob);
-          dispatch({
-            type: "SET_RESULT",
-            payload: { index, result: url },
+        const response = await wretch(
+          `https://filefusion-function.azurewebsites.net/api/fileConverter`
+        )
+          .headers({ "Content-Type": "application/json" })
+          .post({
+            image: encodedImage,
+            outputFormat: file.format,
+            filename,
+          })
+          .json<ConversionResponse>()
+          .catch(() => {
+            dispatch({
+              type: "SET_ERROR",
+              payload: {
+                index,
+                error: "Erro ao converter imagem",
+              },
+            });
           });
-        })
-        .catch((error) => {
-          console.error(error);
-          dispatch({
-            type: "SET_ERROR",
-            payload: { index, error: true },
-          });
-        })
-        .finally(() => {
-          dispatch({ type: "SET_LOADING", payload: { index, loading: false } });
+
+        if (!response) return;
+
+        dispatch({
+          type: "SET_RESULT",
+          payload: {
+            index,
+            result: response.url,
+          },
         });
+      };
     });
   }, [conversionFiles]);
 
@@ -210,56 +235,73 @@ function App() {
         {conversionFiles.length > 0 && (
           <div className="mt-4 ">
             <ul className="mt-2 flex flex-col gap-4">
-              {conversionFiles.map(({ file, format }, index) => (
-                <li
-                  key={file.name}
-                  className="text-sm bg-[#F9F9F9] rounded-xl p-4 flex gap-4 items-center"
-                >
-                  <FileImage size={20} className="text-[#BBC9D7]" />
-
-                  <span className="mr-auto">{file.name}</span>
-
-                  <Select
-                    value={format}
-                    onValueChange={(value: ImageFormat) =>
-                      dispatch({
-                        type: "SET_FORMAT",
-                        payload: { index, format: value },
-                      })
-                    }
+              {conversionFiles.map(
+                ({ file, format, result, loading }, index) => (
+                  <li
+                    key={file.name}
+                    className="text-sm bg-[#F9F9F9] rounded-xl p-4 flex gap-4 items-center"
                   >
-                    <SelectTrigger className="w-[180px]">
-                      <SelectValue placeholder="Formato" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectItem value="png">PNG</SelectItem>
-                      <SelectItem value="jpeg">JPEG</SelectItem>
-                      <SelectItem value="gif">GIF</SelectItem>
-                      <SelectItem value="bmp">BMP</SelectItem>
-                      <SelectItem value="tiff">TIFF</SelectItem>
-                    </SelectContent>
-                  </Select>
+                    <FileImage size={20} className="text-[#BBC9D7]" />
 
-                  {conversionFiles[index].loading && (
-                    <Player src={loading} autoplay loop />
-                  )}
+                    <span className="mr-auto">{file.name}</span>
 
-                  {conversionFiles[index].result && (
-                    <Player src={download} autoplay loop />
-                  )}
+                    <Select
+                      value={format}
+                      onValueChange={(value: ImageFormat) =>
+                        dispatch({
+                          type: "SET_FORMAT",
+                          payload: { index, format: value },
+                        })
+                      }
+                    >
+                      <SelectTrigger className="w-[180px]">
+                        <SelectValue placeholder="Formato" />
+                      </SelectTrigger>
 
-                  {conversionFiles[index].error && (
-                    <Player src={alert} autoplay loop />
-                  )}
-                </li>
-              ))}
+                      {/* TODO: remove the current format of file */}
+                      <SelectContent>
+                        <SelectItem value="image/png">PNG</SelectItem>
+                        <SelectItem value="image/jpeg">JPEG</SelectItem>
+                        <SelectItem value="image/gif">GIF</SelectItem>
+                        <SelectItem value="image/bmp">BMP</SelectItem>
+                        <SelectItem value="image/tiff">TIFF</SelectItem>
+                      </SelectContent>
+                    </Select>
+
+                    {loading && <Player src={loadingAnimation} autoplay loop />}
+
+                    {result && (
+                      <Tooltip delayDuration={0}>
+                        <TooltipTrigger>
+                          <Button
+                            variant="ghost"
+                            onClick={() => downloadRef.current?.play()}
+                          >
+                            <a href={result}>
+                              <Player ref={downloadRef} src={download} />
+                            </a>
+                          </Button>
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>Clique aqui para baixar seu arquivo</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    )}
+
+                    {conversionFiles[index].error && (
+                      <Tooltip delayDuration={0}>
+                        <TooltipTrigger>
+                          <Player src={alert} autoplay loop />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>{conversionFiles[index].error}</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    )}
+                  </li>
+                )
+              )}
             </ul>
-            {/* <p className="mt-6 text-sm text-muted-foreground text-right">
-              {files.length} arquivos selecionados
-            </p> */}
-            {/* <div className="mt-6 text-right">
-              <Button variant="secondary">Enviar</Button>
-            </div> */}
           </div>
         )}
       </section>
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App.tsx";
 import { TelemetryProvider } from "./utils/telemetry.tsx";
+import { TooltipProvider } from "./components/Tooltip.tsx";
 
 import "@fontsource/raleway/500.css";
 import "@fontsource/raleway/600.css";
@@ -12,7 +13,9 @@ import "./styles/global.css";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <TelemetryProvider>
-      <App />
+      <TooltipProvider>
+        <App />
+      </TooltipProvider>
     </TelemetryProvider>
   </React.StrictMode>
 );
